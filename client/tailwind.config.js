8701c18907ca9eb4103cc6574655babd2fc5e7dd
dashleremi <dashleremi@gmail.com>
2024-10-20 09:40:21 -1000

    initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // for React components in src folder
+    './client/index.html', // for any HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
