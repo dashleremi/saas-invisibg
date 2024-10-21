57d54444c77bd4c7cc2baf9dc442c30ea2bed0e6
dashleremi <dashleremi@gmail.com>
2024-10-20 17:27:56 -1000

    image slider, process, images

--- client/src/components/RemovalExample.jsx
+++ client/src/components/RemovalExample.jsx
@@ -2,14 +2,24 @@ import React, {useState} from 'react'
 
 const RemovalExample = () => {
 
-    const [sliderPosition, setSliderPosition] = useState(50)
+    const [sliderPosition, setSliderPosition] = useState(50) // showing 50% of an image
     const handleSliderChange = (e) => {
         setSliderPosition(e.target.value)
     }
 
   return (
     <div className='mx-4 lg:mx-44 py-20 xl:py-40'>
-        <h1 className='font-primary text-center text-xl md:text-2xl lg:text-3xl'>High-Precision <br /> Background Removal for Crisp Results</h1>
+
+        <h1 className='mb-12 sm:mb-24 font-primary text-center text-xl md:text-2xl lg:text-3xl'>High-Precision <br /> Background Removal for Crisp Results</h1>
+
+        {/* background images */}
+        <div className='relative w-full max-w-3xl overflow-hidden m-auto rounded-xl'>
+            <img src="removal-before1.png" style={{clipPath: `inset(0 ${100.2 - sliderPosition}% 0 0)`}} alt="" />
+            <img className='absolute top-0 left-0 w-full h-full' src="removal-after1.png" style={{clipPath: `inset(0 0 0 ${sliderPosition}%)`}}  alt="" />
+
+            {/* image slider for before and after */}
+            <input className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full z-10 slider' type="range" min={0} max={100} value={sliderPosition} onChange={handleSliderChange} />
+        </div>
     </div>
   )
 }
