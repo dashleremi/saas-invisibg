9d21ab6993c367a8cd145725acba1603b41a1eb8
dashleremi <dashleremi@gmail.com>
2024-10-20 22:22:09 -1000

    additional features

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,1 +1,27 @@
-//icons, svgs
+//icons, svgs, testimonials, plans
+
+export const testimonialsData = [
+    {
+        id: 1,
+        text: "This tool saved me hours of editing! The background removal was seamless, and the quality of the images remained perfect.",
+        author: "Jessica R.",
+        image: "/prof-1.PNG",
+        jobTitle: "Content Creator"
+    },
+
+    {
+        id: 2,
+        text: "I was amazed at how quickly and accurately it removed the backgrounds from my product photos. It's like having a professional editor on hand!",
+        author: "Miho S.",
+        image: "/prof-2.PNG",
+        jobTitle: "Business Owner"
+    },
+
+    {
+        id: 3,
+        text: "As a designer, I need clean, high-quality images, and this service delivers every time. It's fast, reliable, and increadibly easy to use.",
+        author: "Maytel G.",
+        image: "/prof-3.PNG",
+        jobTitle: "Graphic Designer"
+    }
+];
