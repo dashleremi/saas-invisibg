8513093371f108d500f8dc867f3686c299447d91
dashleremi <dashleremi@gmail.com>
2024-10-21 12:13:47 -1000

    dotenv update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,12 @@
+import {Webhook} from 'svix'
 // API controller function to manage clerk user with database
 
 // API end point
 // http://localhost:4000/api/user/webhooks
-const clerkWebHooks = async (req, res) => [
-    
-]
+const clerkWebHooks = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
